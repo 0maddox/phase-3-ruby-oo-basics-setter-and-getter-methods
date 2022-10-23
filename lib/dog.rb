23b87009b86ee6a4db952a310682336fa64c7602
nicholas <nickkiim7@gmail.com>
2022-10-24 00:50:03 +0300

    completed assighnment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    # name setter 
+    def name
+        @name
+    end
+
+    # breed getter 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # breed setter 
+    def breed
+        @breed
+end
 end
